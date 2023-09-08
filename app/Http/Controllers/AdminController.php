6e6a2b000fcd8845b4fcaa9f617df3ad6b8982a8
Lorenzo Ricciardiello <lorenzo1995111@gmail.com>
2023-09-08 18:13:58 +0100

    dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function dashboard(){
+        $users = User::all();
         $products = Product::all();
-        return view('admin.dashboard',compact('products'));
+        return view('admin.dashboard',compact('products','users'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -49,4 +49,58 @@
 </div>
 </div>
 
+
+
+
+
+
+
+<div class="container my-4">
+        <div class="row">
+            <h4>Tutti gli utenti</h4>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Email</th>
+                            <!-- <th scope="col">Ruolo</th> -->
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }} </td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <!-- <form action="" method="POST">
+                                        @csrf
+                                        <button class="btn btn-primary">User</button>
+                                        <input type="hidden" value="user" name="role">
+                                    </form>
+                                   
+                                    <form action="" method="POST">
+                                        @csrf
+                                        <button class="btn btn-secondary">Admin</button>
+                                        <input type="hidden" value="admin" name="role">
+                                    </form> -->
+
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
 </x-layout>
